1a7e29e3c241237da689b376685bd46fb58bb7fd
WEI TSE <curry900312@gmail.com>
2023-06-18 14:24:23 +0800

    add email submit

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -1,28 +1,7 @@
 import React, { useRef } from "react";
-import emailjs from "@emailjs/browser";
 
 function Contact() {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        "YOUR_SERVICE_ID",
-        "YOUR_TEMPLATE_ID",
-        form.current,
-        "YOUR_PUBLIC_KEY"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+  const formRef = useRef(null);
 
   return (
     <section id="contact" className="h-screen bg-contact md:p-10">
@@ -34,12 +13,12 @@ function Contact() {
           <p className="text-4xl my-1">Lets Contact Me!</p>
           <p className="text-purple-500 my-5">Let's create something together!</p>
         </div>
-        <form ref={form} onSubmit={sendEmail} className="flex flex-col justify-center h-full basis-1/2 py-24 px-8 bg-white md:border-2 md:border-gray-300 md:rounded-xl md:h-auto md:py-8">
+        <form action="https://formsubmit.co/8caa425b2ae3afbd1d62fd8e66b25052" method="POST" ref={formRef} className="flex flex-col justify-center h-full basis-1/2 py-24 px-8 bg-white md:border-2 md:border-gray-300 md:rounded-xl md:h-auto md:py-8">
           <div className="text-4xl font-outfit font-bold my-3">Send me a message</div>
           <div className="flex flex-col gap-3 font-outfit font-bold">
-            <input type="text" name="user_name" placeholder="Full name*" className="bg-gray-100 py-2 px-4 rounded-md"/>
-            <input type="email" name="user_email" placeholder="Email address*" className="bg-gray-100 py-2 px-4 rounded-md"/>
-            <input type="text" name="user_subject" placeholder="Subject" className="bg-gray-100 py-2 px-4 rounded-md"/>
+            <input type="text" name="name" placeholder="Full name*" className="bg-gray-100 py-2 px-4 rounded-md"/>
+            <input type="email" name="email" placeholder="Email address*" className="bg-gray-100 py-2 px-4 rounded-md"/>
+            <input type="text" name="subject" placeholder="Subject" className="bg-gray-100 py-2 px-4 rounded-md"/>
             <label>Tell me more about your thought*</label>
             <textarea name="message" rows="5" className="bg-gray-100 py-2 px-4 rounded-md"/>
           </div>
