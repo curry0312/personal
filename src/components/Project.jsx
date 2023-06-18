1f2e1649f1359dbd715ea7219a5b8572de0f8def
WEI TSE <curry900312@gmail.com>
2023-06-18 15:47:41 +0800

    a href add target=_blank

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,7 +10,7 @@ function Project({ image, description, url , variants }) {
         className="object-cover w-full h-full"
       />
 
-      <a href={url} className="absolute hidden justify-center items-center -z-30 text-white bg-purple-600 opacity-90 inset-0 group-hover:flex group-hover:z-20">
+      <a href={url} target="_blank" className="absolute hidden justify-center items-center -z-30 text-white bg-purple-600 opacity-90 inset-0 group-hover:flex group-hover:z-20">
         <p className="font-outfit w-[70%] text-center">{description}</p>
       </a>
     </motion.div>
